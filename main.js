a0e2192b36649bea63b57ba193a8607ed7516b67
question.mark.Qu.rk@gmail.com <question.mark.Qu.rk@gmail.com>
2023-12-28 23:51:07 +0900

    waaaa

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+const lol = document.getElementById("LOOOL");
+const button = document.getElementById("button");
+button.addEventListener('click', function() {
+  lol.style.display= "block";
+})
